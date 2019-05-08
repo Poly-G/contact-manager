f80c72b4ab88d53772b44e220a56d1df56b316c0
Poly Goveia <poly.goveia@gmail.com>
2019-05-08 13:26:46 -0700

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+
 function App() {
   return (
     <div className="App">
-    <h1>The App Component</h1>
+      <Header />
+      <Contact />
     </div>
   );
 }
