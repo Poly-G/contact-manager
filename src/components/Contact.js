e4f41466bd03015f1147ff915e84a72aa8607615
Poly Goveia <poly.goveia@gmail.com>
2019-05-09 18:26:39 -0700

    add toggle contact functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,10 +17,12 @@ onShowClick = () => {
       return (
           <div className="card card-body mb-3">
               <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down" /> </h4>
-              <ul className="list-group">
+              {this.state.showContactInfo ? (
+                  <ul className="list-group">
                   <li className="list-group-item">Email: {email}</li>
                   <li className="list-group-item">Phone: {phone}</li>
               </ul>
+              ) : null}
           </div>
       )
   }
