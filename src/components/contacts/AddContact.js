7c3ac7259925266d6d5f09e8164e7305d284197c
Poly Goveia <poly.goveia@gmail.com>
2019-05-29 14:28:05 -0700

    add contact functionality

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Consumer } from '../../context';
+import uuid from 'uuid';
 
 class AddContact extends Component {
     state = {
@@ -7,54 +9,89 @@ class AddContact extends Component {
         phone: ''
     }
 
+    onSubmit = (dispatch,e) => {
+        e.preventDefault();
+
+        const { name, email, phone } = this.state;
+
+        const newContact = {
+            id: uuid(),
+            name, 
+            email, 
+            phone
+        };
+
+        dispatch({type: 'ADD_CONTACT', payload: newContact})
+    }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
     render() {
-        return (
-            <div className='card mb-3'>
-                <div className="card-header">Add Contact
+        const { name, email, phone } = this.state;
 
-                </div>
-                <div className="card-body">
-                    <form>
-                        {/* Name input */}
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input 
-                            type="text" 
-                            name="name"
-                            className="form-control form-control-lg"
-                            placeholder="Enter Name..."
-                            />
+        return (
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className='card mb-3'>
+                            <div className="card-header">Add Contact
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                                    {/* Name input */}
+                                    <div className="form-group">
+                                        <label htmlFor="name">Name</label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter Name..."
+                                            value={name}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    {/* Email input */}
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter email..."
+                                            value={email}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    {/* Phone number input */}
+                                    <div className="form-group">
+                                        <label htmlFor="Phone Number...">Phone Number</label>
+                                        <input
+                                            type="text"
+                                            name="phone"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter Phone Number"
+                                            value={phone}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    {/* Submit input */}
+                                    <input
+                                        type="submit"
+                                        value="Add Contact"
+                                        className="btn btn-light btn-block"
+                                    />
+                                </form>
+                            </div>
+
                         </div>
-                        {/* Email input */}
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                            type="email"
-                            name="email"
-                            className="form-control form-control-lg"
-                            placeholder="Enter email..."
-                            />
-                        </div>
-                        {/* Phone number input */}
-                        <div className="form-group">
-                            <label htmlFor="Phone Number...">Phone Number</label>
-                            <input
-                            type="number"
-                            name="phone"
-                            className="form-control form-control-lg"
-                            placeholder="Enter Phone Number"
-                            />
-                        </div>
-                        <input 
-                        type="submit"
-                        value="Add Contact"
-                        className="btn btn-light btn-block"
-                        />
-                    </form>
-                </div>
-                
-            </div>
+                    )
+                }}
+            </Consumer>
         )
     }
 }
