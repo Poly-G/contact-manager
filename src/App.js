5ef2b5db349f60f1474e60186771fe898caa344f
Poly Goveia <poly.goveia@gmail.com>
2019-05-31 16:12:08 -0700

    add initial routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+// Components
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import Provider from './context';
 import AddContact from './components/contacts/AddContact';
+import About from './components/pages/About';
 
 
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <AddContact />
-          <Contacts />
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route
+              exact path="/"
+              component={Contacts}
+              />
+              <Route
+              exact path="/contact/add"
+              component={AddContact}
+              />
+              <Route
+              exact path="/about"
+              component={About}
+              />
+              
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     </Provider>
   );
 }
